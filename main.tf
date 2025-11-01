d5642256138d81b088649c0d48d9cc96594d6c20
karthik18200301 <madinenikarthikeyan@gmail.com>
2025-11-01 15:54:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-  count                  = 4
-  ami                    = "ami-0360c520857e3138f"
-  instance_type          = "t2.medium"
-  key_name               = "Netflix"
-  vpc_security_group_ids = ["sg-0ead21b7862e54f9a"]
+  count                  = 3
+  ami                    = "ami-02b8269d5e85954ef"
+  instance_type          = "t3.medium"
+  key_name               = "SSS"
+  vpc_security_group_ids = ["sg-027bb845ecfe37fa7"]
   tags = {
     Name = var.instance_names[count.index]
   }
 }
 
 variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+  default = ["jenkins", "tomcat-1", "Monitoring server"]
 }
